524ed03b22306f588509d7b184b7ac5bf4f8dde3
Sweper27 <rifqiariessaputra@gmail.com>
2025-04-16 11:25:18 +0700

    menambah data service transaksi

--- CM1_ASD25_12_26/src/bankdomain.java
+++ CM1_ASD25_12_26/src/bankdomain.java
@@ -3,8 +3,13 @@ import java.util.Scanner;
 public class bankdomain {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        serviceTransaksi bank = serviceTransaksi(5);
+        serviceTransaksi serviceTransaksi = new serviceTransaksi(5) ;
 
+        bank trs1 =new bank("16030927 3084", "Wallace", "Mei Mei", "082-458-264-3263", "wallace@gmail.com");
+        bank trs2 =new bank("16100617 0573", "Darius", "Susanti", "081-357-843-0547", "darius@pt.org");
+        bank trs3 =new bank("16240401 2243", "Fuller", "Rosalia", "085-556-712-7062", "fuller@mail.com");
+        bank trs4 =new bank("16270525 0112", "Maria", "Krabela", "082-267-223-0234", "mari@gmail.com");
+        bank trs5 =new bank("16101617 2416", "Gery", "Fatimah", "083-683-416-8323", "gery@gery.com");
         while (true) {
             System.out.println("==================POLINEMA BANK==================");
             System.out.println("\nBankMenu:");
@@ -21,10 +26,9 @@ public class bankdomain {
             
             switch (pilihan) {
                 case 1:
-                    
-                    break;
-                    
-                    case 2:
+                
+                    break;  
+                case 2:
                     
                     break;
                     
@@ -51,10 +55,5 @@ public class bankdomain {
                     System.out.println("Pilihan tidak valid!");
             }
         }
-    }
-
-    private static serviceTransaksi serviceTransaksi(int i) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'serviceTransaksi'");
-    }
+    }  
 }
--- CM1_ASD25_12_26/src/serviceTransaksi.java
+++ CM1_ASD25_12_26/src/serviceTransaksi.java
@@ -1,15 +1,49 @@
 public class serviceTransaksi {
-    transaksi [] Transaksi;
-        int idx;
+ transaksi [] Transaksi;
+ int idx;
     
-        public void ServiceTransaksi(int kapasitas) {
-            Transaksi = new transaksi[5];
-            idx = 0;
-        }
-        public void tambah(transaksi trs) {
-            if (idx < Transaksi.length) {
-                Transaksi[idx] = trs;
-                idx++;
+    
+
+    public serviceTransaksi(int kapasitas) {
+        Transaksi = new transaksi[kapasitas];
+    }
+    
+    public void displayData() {
+        for (transaksi transaksi : Transaksi) {
+                if (transaksi != null) {
+                    transaksi.tampilTransaksi();
             }
         }
+    }
+    public void searchByNoRekening(String noRekening) {
+        for (transaksi transaksi : Transaksi) {
+            if (Transaksi != null) {
+                for (bank bank : transaksi.bankAcc) {
+                    if (bank.noRekening.equals(noRekening)) {
+                        transaksi.tampilTransaksi();
+                    }
+                }
+            }
+        }
+    }
+    public double findMaxSaldo() {
+        double maxSaldo = Double.MIN_VALUE;
+        for (transaksi transaksi : Transaksi) {
+            if (transaksi != null && transaksi.finalSaldo > maxSaldo) {
+                maxSaldo = transaksi.finalSaldo;
+            }
+        }
+        return maxSaldo;
+    }
+    public void sorting() { 
+        for (int i = 0; i < idx - 1; i++) {
+            for (int j = 0; j < idx - i - 1; j++) {
+                if (Transaksi[j].inOutSaldo > Transaksi[j + 1].inOutSaldo) {
+                    transaksi temp = Transaksi[j];
+                    Transaksi[j] = Transaksi[j + 1];
+                    Transaksi[j + 1] = temp;
+                }
+            }
+        }
+    }
 }
