8f8b51abb46baccffa2386121b800f6bbb6e1815
Jasmine Nasywa Nabilah <jasminenasywa6@gmail.com>
2025-04-16 10:05:53 +0700

    transaksi

--- CM1_ASD25_12_26/src/transaksi.java
+++ CM1_ASD25_12_26/src/transaksi.java
@@ -1,3 +1,36 @@
 public class transaksi {
-    
+    String kodeTransaksi;
+    double saldo;
+    double inOutSaldo;
+    double finalSaldo;
+    String tanggalTransaksi;
+    String type;
+    int idx;
+
+    bank[] bankAcc;
+
+    public transaksi(String kodeTransaksi, double saldo, double inOutSaldo, double finalSaldo, String tanggalTransaksi, String type, bank[] bankAcc){
+        this.kodeTransaksi = kodeTransaksi;
+        this.saldo = saldo;
+        this.inOutSaldo = inOutSaldo;
+        this.finalSaldo = finalSaldo;
+        this.tanggalTransaksi = tanggalTransaksi;
+        this.type = type;
+        this.bankAcc = bankAcc;
+    }
+
+    public void tampilTransaksi(){
+        System.out.println("Kode Transaksi: " + kodeTransaksi);
+        System.out.println("Saldo: " + saldo);
+        System.out.println("In/Out Saldo: " + inOutSaldo);
+        System.out.println("Final Saldo: " + finalSaldo);
+        System.out.println("Tanggal Transaksi: " + tanggalTransaksi);
+        System.out.println("Type: " + type);
+        System.out.println("Bank Accounts:");
+        for (bank bank : bankAcc) {
+            bank.tampil();
+        }
+        System.out.println("---------------------");
+    }
+
 }
