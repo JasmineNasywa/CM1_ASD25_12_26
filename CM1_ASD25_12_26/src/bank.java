028e6e1fe6f6ec9620b78df8d3a1ce4d4ead996c
Sweper27 <rifqiariessaputra@gmail.com>
2025-04-16 09:32:39 +0700

    pengubahan bank

---
+++ CM1_ASD25_12_26/src/bank.java
@@ -0,0 +1,20 @@
+public class bank {
+    String noRekening;
+    String nama;  
+    String namaIbu; 
+    String noHp; 
+    String email;
+    
+    public bank(String nk, String name, String ibu, String notlp, String email) {
+        this.noRekening = nk;
+        this.nama = name;
+        this.namaIbu = ibu;
+        this.noHp = notlp;
+        this.email =email;
+    }
+    public void tampil() {
+        System.out.println("NoRekening " + "\t" + noRekening + "Nama: " + "\t" + nama + "Nama Ibu: " + "\t" + namaIbu + "NoHP: " + "\t" + noHp + "email" + "/t" + email);
+        System.out.println("---------------------");
+    }
+
+}
---
+++ CM1_ASD25_12_26/src/serviceTransaksi.java
@@ -0,0 +1,5 @@
+public class serviceTransaksi {
+    public static void main(String[] args) {
+        
+    }
+}
