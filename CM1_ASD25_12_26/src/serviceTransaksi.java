59e750eb6ed113268cd0c03b20a2c2164b36a818
Sweper27 <rifqiariessaputra@gmail.com>
2025-04-16 10:24:34 +0700

    menambah data di service transaksi

--- CM1_ASD25_12_26/src/serviceTransaksi.java
+++ CM1_ASD25_12_26/src/serviceTransaksi.java
@@ -1,5 +1,15 @@
 public class serviceTransaksi {
-    public static void main(String[] args) {
-        
-    }
+    transaksi [] Transaksi;
+        int idx;
+    
+        public void ServiceTransaksi(int kapasitas) {
+            Transaksi = new transaksi[5];
+            idx = 0;
+        }
+        public void tambah(transaksi trs) {
+            if (idx < Transaksi.length) {
+                Transaksi[idx] = trs;
+                idx++;
+            }
+        }
 }
